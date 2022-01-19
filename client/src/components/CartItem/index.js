f8ef197edd4f1ef4b1e870d017037b54438b3357
Dave Quinn <dquinn8@cogeco.ca>
2022-01-19 12:41:27 -0500

    added link to product page from shopping cart product image

--- client/src/components/CartItem/index.js
+++ client/src/components/CartItem/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
+import { Link } from "react-router-dom";
 
 const CartItem = ({ item }) => {
   return (
     <div className="flex-row">
       <div>
+      <Link to={`/products/${item._id}`}>
         <img
+          alt={item.name}
           src={`/images/${item.image}`}
-          alt=""
         />
+      </Link>
       </div>
       <div>
         <div>{item.name}, ${item.price}</div>
