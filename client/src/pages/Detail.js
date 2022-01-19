6a41c564d7992ad11d31f2d4d421b64159cac48c
Dave Quinn <dquinn8@cogeco.ca>
2022-01-19 12:37:14 -0500

    added remove from cart functionality to the Detail component

--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -52,6 +52,13 @@ function Detail() {
     }
   };
 
+  const removeFromCart = () => {
+    dispatch({
+      type: REMOVE_FROM_CART,
+      _id: currentProduct._id
+    });
+  };
+
   return (
     <>
       {currentProduct ? (
@@ -65,7 +72,12 @@ function Detail() {
           <p>
             <strong>Price:</strong>${currentProduct.price}{' '}
             <button onClick={addToCart}>Add to Cart</button>
-            <button>Remove from Cart</button>
+            <button 
+              disabled={!cart.find(p => p._id === currentProduct._id)} 
+              onClick={removeFromCart}
+            >
+              Remove from Cart
+            </button>
           </p>
 
           <img
