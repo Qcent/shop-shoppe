166c160697eba7e9b4ebf1af78f370b1405ece8e
Dave Quinn <dquinn8@cogeco.ca>
2022-01-20 12:47:21 -0500

    updated checkout resolver ot user referrer url during success/cancel and included image link for line items

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -7,6 +7,7 @@ const stripe = require('stripe')('sk_test_4eC39HqLyjWDarjtT1zdp7dc');
 const resolvers = {
     Query: {
         checkout: async(parent, args, context) => {
+            const url = new URL(context.headers.referer).origin;
             const order = new Order({ products: args.products });
             const { products } = await order.populate('products').execPopulate();
 
@@ -16,7 +17,8 @@ const resolvers = {
                 // generate product id
                 const product = await stripe.products.create({
                     name: products[i].name,
-                    description: products[i].description
+                    description: products[i].description,
+                    images: [`${url}/images/${products[i].image}`]
                 });
 
                 // generate price id using the product id
@@ -37,8 +39,8 @@ const resolvers = {
                 payment_method_types: ['card'],
                 line_items,
                 mode: 'payment',
-                success_url: 'https://example.com/success?session_id={CHECKOUT_SESSION_ID}',
-                cancel_url: 'https://example.com/cancel'
+                success_url: `${url}/success?session_id={CHECKOUT_SESSION_ID}`,
+                cancel_url: `${url}/`
             });
 
             return { session: session.id };
